4ec362ef221f267e6c2f626a1c52ebc65a820233
Pratik Keluskar <65060276+Pratik-1997@users.noreply.github.com>
2022-12-03 20:16:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+   
+   // typing text animation scripta
+    var typed = new Typed(".typing", {
+        strings: ["Designer", "Wordpress Dev", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Designer", "Wordpress Dev", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
